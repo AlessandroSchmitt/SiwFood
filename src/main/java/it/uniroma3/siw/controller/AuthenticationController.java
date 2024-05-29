c1b972ba0362ceef5b73831afd65925af6f9b5bd
alessandroSania <alessandroschmitt03@gmail.com>
2024-05-29 22:36:21 +0200

    ho migliorato la porco dio navbar

--- src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
+++ src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -56,6 +56,9 @@ public class AuthenticationController {
             if (credenziali.getRuolo().equals(Credenziali.ADMIN_ROLE)) {
                 return "admin/indexAdmin.html";
             }
+            if (credenziali.getRuolo().equals(Credenziali.COUCO_ROLE)) {
+                return "cuoco/indexCuoco.html";
+            }
         }
         return "index.html";
     }
--- src/main/java/it/uniroma3/siw/controller/GlobalController.java
+++ src/main/java/it/uniroma3/siw/controller/GlobalController.java
@@ -1,16 +1,25 @@
 package it.uniroma3.siw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import it.uniroma3.siw.model.Credenziali;
+import it.uniroma3.siw.model.Cuoco;
+import it.uniroma3.siw.service.CredenzialiService;
+
 import org.springframework.security.core.userdetails.UserDetails;
 
 @ControllerAdvice
 public class GlobalController {
-	@ModelAttribute("UserDetails")
+	@Autowired
+	private CredenzialiService credenzialiService;
 	
+	@ModelAttribute("userDetails")
 	public UserDetails getUtente() {
 		UserDetails  utente = null;
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -18,4 +27,29 @@ public class GlobalController {
 			utente = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();}
 		return utente;
 	}
+	
+    @ModelAttribute("ruolo")
+    public String getAuthorityAsString() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && !authentication.getAuthorities().isEmpty()) {
+            GrantedAuthority authority = authentication.getAuthorities().iterator().next();
+            return authority.getAuthority();
+        }
+        return null;
+    }
+    
+    
+    @ModelAttribute("cuocoCorrente")
+    public Cuoco getCuoco() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() 
+                && !(authentication instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            Credenziali credenziali = credenzialiService.getCredenziali(userDetails.getUsername());
+            if (credenziali != null) {
+                return credenziali.getCuoco();
+            }
+        }
+        return null;
+    }
 }
