3612b11b42a33531c72385f54d771d1d6cbc7f75
alessandroSania <alessandroschmitt03@gmail.com>
2024-05-30 17:58:44 +0200

    aggiornata l'aggiunta dell'ingrediente

---
+++ src/main/java/it/uniroma3/siw/controller/IngredienteController.java
@@ -0,0 +1,30 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.uniroma3.siw.service.IngredienteService;
+
+import java.util.List;
+
+@Controller
+public class IngredienteController {
+
+    @Autowired
+    private IngredienteService ingredienteService;
+
+    @GetMapping("/addIngredienti")
+    public String showAddIngredienteForm(Model model) {
+        return "addIngredienti";
+    }
+
+    @PostMapping("/aggiungiIngredienti")
+    public String addIngrediente(@RequestParam("ingredienti") List<String> ingredienti, Model model) {
+        ingredienteService.addIngredienti(ingredienti);
+        return "redirect:/cuoco/aggiungiRicetta";
+    }
+}
