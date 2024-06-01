edbc9dc46cac5827add50d754bd23ccc35cc4006
alessandroSania <alessandroschmitt03@gmail.com>
2024-06-01 15:54:30 +0200

    ritorna qui!!

--- src/main/java/it/uniroma3/siw/Authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/Authentication/AuthConfiguration.java
@@ -45,7 +45,7 @@ public class AuthConfiguration {
 	// .requestMatchers("/**").permitAll()
 	// chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
 
-	.requestMatchers(HttpMethod.GET, "/", "/index", "/register", "/css/**", "/images/**","/cuochiAggiunti/**" , "static/images/favicon.ico", "/cuochi", "/ricette", "/cuoco/**", "/ricetta/**").permitAll()
+	.requestMatchers(HttpMethod.GET, "/", "/index", "/register", "/css/**", "/images/**","/cuochiAggiunti/**" , "/ricetteCuoco/**" , "static/images/favicon.ico", "/cuochi", "/ricette", "/cuoco/**", "/ricetta/**").permitAll()
 
 
 	// chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register
